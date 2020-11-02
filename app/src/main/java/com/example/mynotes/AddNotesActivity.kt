d7e399e15bb2e305ccdb9c74c9b1f3d96060110e
leoprananta <leonantapramudya7@gmail.com>
2020-11-02 19:15:25 +0700

    Save Data to Firebase

--- app/src/main/java/com/example/mynotes/AddNotesActivity.kt
+++ app/src/main/java/com/example/mynotes/AddNotesActivity.kt
@@ -1,15 +1,27 @@
 package com.example.mynotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mynotes.model.Notes
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_add_notes.*
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddNotesActivity : AppCompatActivity() {
+
+    lateinit var ref : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
+
+        ref = FirebaseDatabase.getInstance().getReference("NOTES")
 
         val back = findViewById<ImageView>(R.id.back_add)
         val btnAdd = findViewById<Button>(R.id.btn_add)
@@ -19,8 +31,27 @@ class AddNotesActivity : AppCompatActivity() {
         }
 
         btnAdd.setOnClickListener {
-            startActivity<MainActivity>()
+            saveData()
         }
 
+    }
+
+    private fun saveData() {
+        val title = editText_addActivity_notesTitle.text.toString()
+        val description = editText_addActivity_notesDescription.text.toString()
+
+        val sdf = SimpleDateFormat("dd/MMM/yyyy")
+        val date = sdf.format(Date())
+
+        val notes = Notes(title, description, date)
+        val notesId = ref.push().key.toString()
+
+        ref.child(notesId).setValue(notes).addOnCompleteListener {
+            toast("Berhasil")
+            editText_addActivity_notesTitle.setText("")
+            editText_addActivity_notesDescription.setText("")
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/mynotes/model/Notes.kt
@@ -0,0 +1,5 @@
+package com.example.mynotes.model
+
+class Notes(var title: String, var description: String, var date: String) {
+    constructor(): this("","", "")
+}
